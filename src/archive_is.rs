21b140eb3da27440e5a39c7a939de1bfd687d4ee
Konstantin Ilchenko <kyle.mom.is.awesome@gmail.com>
2016-03-29 21:25:20 +0300

    Probably working version

--- src/archive_is.rs
+++ src/archive_is.rs
@@ -3,45 +3,65 @@ extern crate hyper;
 extern crate regex;
 
 use std::io::Read;
+use std::time::Duration;
+use std::error::Error;
 use iron::prelude::*;
-use router::Router;
 use hyper::Client;
 use hyper::header::{Headers, UserAgent, ContentType};
 
 
 const USER_AGENT: &'static str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2687.0 Safari/537.36";
 const ARCHIVE_IS: &'static str = "http://archive.is/";
+const TIMEOUT: u64 = 6;
+
 
 header!{ (Link   , "Link"   ) => [String] }
 header!{ (Refresh, "Refresh") => [String] }
 
-// #todo link parse, use post, json, timeouts, error handling, log
+
+// todo: use regex! macro (works only in nightly by now)
+// todo: find a way to set up timeout without config parsing. const mut is not a option since it requires unsafe block
 
 
 fn get_user_agent() -> UserAgent {
     return UserAgent(String::from(USER_AGENT));
 }
 
-fn get_token() -> String {
+fn set_client_defaults(client: &mut Client) {
+    let timeout = Some(Duration::from_secs(TIMEOUT));
+
+    client.set_read_timeout(timeout);
+    client.set_write_timeout(timeout);
+}
+
+fn get_token() -> Result<String, String> {
+    let mut client = Client::new();
+    set_client_defaults(&mut client);
+
     let mut headers = Headers::new();
     headers.set(get_user_agent());
 
-    let mut res = Client::new()
-        .get(ARCHIVE_IS)
-        .headers(headers)
-        .send()
-        .unwrap();
+    let mut res = match client.get(ARCHIVE_IS).headers(headers).send() {
+        Ok(resp) => resp,
+        Err(why) => return Err(why.description().into())
+    };
 
     let mut body = String::new();
-    res.read_to_string(&mut body).unwrap();
+    match res.read_to_string(&mut body) {
+        Err(why) => return Err(why.description().into()),
+        Ok(_)    => {}
+    };
 
-    let re = regex::Regex::new("name=\"submitid\" value=\"([^\"]+)\"").unwrap();
-    let cap = re.captures(&body).unwrap();
+    let re = regex::Regex::new(r#"name="submitid" value="([^"]+)""#).unwrap();
+    let cap = match re.captures(&body) {
+        Some(val) => val,
+        None      => return Err("Token not found".into())
+    };
 
-    return String::from(cap.at(1).unwrap());
+    return Ok(cap.at(1).unwrap().into());
 }
 
-fn submit_with_token(token: String, url: String) -> String {
+fn submit_with_token(token: String, url: String) -> Result<String, String> {
     let mut headers = Headers::new();
     headers.set(get_user_agent());
     headers.set(ContentType::form_url_encoded());
@@ -49,31 +69,49 @@ fn submit_with_token(token: String, url: String) -> String {
     let form = format!("url={0}&submitid={1}", url, token);
     let action = format!("{0}submit/", ARCHIVE_IS);
 
-    let mut res = Client::new()
-        .post(&action)
-        .headers(headers)
-        .body(&form)
-        .send()
-        .unwrap();
+    let mut client = Client::new();
+    set_client_defaults(&mut client);
 
-    if (res.headers.has::<Refresh>()) {
+    let mut res = match client.post(&action).headers(headers).body(&form).send() {
+        Ok(val)  => val,
+        Err(why) => return Err(why.description().into())
+    };
 
-    } else if (res.headers.has::<Link>()) {
+    if res.headers.has::<Refresh>() {
+        let val: Vec<&str> = res.headers.get::<Refresh>().unwrap().split(";").collect();
+        if val.len() == 2 {
+            return Ok(String::from(val[1]).replace("url=", ""));
+        }
+    } else if res.headers.has::<Link>() {
         let mut body = String::new();
-        res.read_to_string(&mut body).unwrap();
-    } else {
+        match res.read_to_string(&mut body) {
+            Err(why) => return Err(why.description().into()),
+            Ok(_)    => {}
+        };
 
+        let re = regex::Regex::new(r#"<link\s+rel="canonical"\s+href="([^"]+)""#).unwrap();
+        let caps = re.captures(&body);
+        if caps.is_some() {
+            return Ok(caps.unwrap().at(1).unwrap().into());
+        }
     }
 
-    return String::new();
+    return Err("Can't parse response".into());
 }
 
 pub fn handle(req: &mut Request) -> IronResult<Response> {
-    let token = get_token();
+    let mut input_link = String::new();
+    if req.body.read_to_string(&mut input_link).unwrap_or(0) == 0 {
+        return Ok(Response::with((iron::status::BadRequest, "Body is empty or cannot be read")));
+    }
 
-    let url = String::from(req.extensions.get::<Router>().unwrap().find("url").unwrap());
+    let token = match get_token() {
+        Ok(val)  => val,
+        Err(why) => return Ok(Response::with((iron::status::ServiceUnavailable, why)))
+    };
 
-    let body = submit_with_token(token, url);
-
-    return Ok(Response::with((iron::status::Ok, body)));
+    match submit_with_token(token, input_link) {
+        Ok(val)  => return Ok(Response::with((iron::status::Ok, val))),
+        Err(why) => return Ok(Response::with((iron::status::ServiceUnavailable, why)))
+    };
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,58 @@
 extern crate iron;
 extern crate router;
+extern crate logger;
+extern crate config;
+extern crate unicase;
 #[macro_use] extern crate hyper;
 
+use std::path::{ Path, PathBuf };
 use iron::prelude::*;
-use router::Router;
+use unicase::UniCase;
 
 mod archive_is;
 
-fn main() {
-    let mut router = Router::new();
-    router.get("/archive.is/:url", archive_is::handle);
 
-    Iron::new(router).http("localhost:3000").unwrap();
+fn main() {
+    let mut args = std::env::args();
+    let cfg_path = args.nth(1).unwrap_or("config.cfg".into());
+    let conf = config::reader::from_file(Path::new(&cfg_path)).unwrap();
+
+    let mut router = router::Router::new();
+    router.post("/archive.is", archive_is::handle);
+
+    let mut chain = Chain::new(router);
+
+    let (logger_before, logger_after) = logger::Logger::new(None);
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
+
+    chain.link_after(CorsMiddleware);
+
+    let listen = conf.lookup_str("listen").unwrap();
+    let threads = conf.lookup_integer32("threads").unwrap() as usize;
+    let protocol = iron::Protocol::Https {
+        certificate: PathBuf::from(conf.lookup_str("ssl_cer").unwrap()),
+        key        : PathBuf::from(conf.lookup_str("ssl_key").unwrap())
+    };
+
+    let iron = Iron::new(chain).listen_with(listen, threads, protocol, None);
+
+    if iron.is_err() {
+        println!("{}", "Well, fug.");
+    }
+}
+
+
+struct CorsMiddleware;
+
+impl iron::AfterMiddleware for CorsMiddleware {
+    fn after(&self, _: &mut Request, mut res: Response) -> IronResult<Response> {
+        res.headers.set(hyper::header::AccessControlAllowOrigin::Any);
+        res.headers.set(
+            hyper::header::AccessControlAllowHeaders(vec![
+                UniCase("Content-Type".to_owned())
+            ])
+        );
+        Ok(res)
+    }
 }
